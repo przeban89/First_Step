f6ff2b7ecd62a86237aff41ece5534d62e1f4a9f
Przemysław Banasiewicz <58090454+przeban89@users.noreply.github.com>
2019-11-29 19:53:34 +0100

    Delete splata_kredytu.py

--- splata_kredytu.py
+++
@@ -1,7 +0,0 @@
-import Definition
-while(True):
-    k = float(input("Podaj kwotę kredytu: "))
-    o = float(input("Podaj oprocentowanie: "))
-    m = float(input("Podaj liczbę miesięcy kredytowania: "))
-    x = Definition.splata(k, o, m)
-    print("Kwota kapitałowa do spłaty: ", round(x, 2))
