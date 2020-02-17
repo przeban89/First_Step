570a0fa037d808da4e9d801dcc810abe0e9ff4d2
Przemysław Banasiewicz <przemyslaw.banasiewicz@gmail.com>
2020-02-17 21:01:15 +0100

    Game. Added funcion count point to player and computer.

--- Game.py
+++ Game.py
@@ -1,39 +1,46 @@
 from enum import IntEnum
 from random import randint
 
-# list with selected option
-# choice = IntEnum('choice', 'Rock, Paper, Scissors')
+# dict with selected option
+
 choice = {1: 'Rock', 2: 'Paper', 3: 'Scissors'}
 # computer choice
 computer = choice.get(randint(1, 3))
-
-player = False
-while not player:
-    print(computer)
-    userInput = int(input('1. Rock, 2. Paper, 3. Scissors? '))
+player_point = 0
+computer_point = 0
+#player = False
+while (player_point < 3) and (computer_point < 3):
+    userInput = int(input('1. Rock, 2. Paper, 3. Scissors? You play to 3 points: '))
     player = choice.get(userInput)
     try:
         if player == computer:
-            print("Tie!")
+            print("Tie!, no one gat point")
         elif player == 'Rock':
             if computer == 'Paper':
-                print('You lose ', computer, 'covers', player)
+                computer_point += 1
+                print('You lose ', computer, 'covers', player, "and get's point")
             else:
-                print("You win!", player, "smashes", computer)
+                player_point += 1
+                print("You win!", player, "smashes", computer, "and you get a point")
         elif player == 'Scissors':
             if computer == 'Rock':
-                print('You lose', computer, 'smashes', player)
+                computer_point += 1
+                print('You lose', computer, 'smashes', player, "and get's point")
             else:
-                print('You win!', player, 'cut', computer)
+                player_point += 1
+                print('You win!', player, 'cuts', computer, "and you get a point")
         elif player == 'Paper':
             if computer == 'Scissors':
-                print('You lose ', computer, 'smashes', player)
+                computer_point += 1
+                print('You lose ', computer, 'cuts', player, "and get's point")
             else:
-                print("You win!", player, 'covers', computer)
+                player_point += 1
+                print("You win!", player, 'covers', computer, "and you get a point")
         else:
             print('Something goes wrong, try again! ')
+        print("Player's points:", player_point, "Computer points: ", computer_point)
     except ValueError:
         print('Non-numeric data found in the file.')
 
-    player = False
+    #player = False
     computer = choice.get(randint(1, 3))
